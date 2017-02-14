9cd472f747c4c0ca2f034fc1d643e888481a7a3a
Dosenoeffner69 <marvin4797@gmail.com>
2017-02-14 10:57:31 +0100

    Test

--- frmWinSnake.Designer.cs
+++ frmWinSnake.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(278, 244);
             this.Name = "frmSnake";
-            this.Text = "Form1";
+            this.Text = "Snake";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
